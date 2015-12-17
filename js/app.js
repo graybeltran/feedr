e80971f8bdad3947d1164b094307674fd3f39480
graybeltran <gray.beltran@gmail.com>
2015-12-16 20:19:52 -0500

    reddit api test

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
 /*
   Please add all Javascript code to this file.
 */
+
+'use strict';
+(function() {
+
+  var redditURL = "https://www.reddit.com/top.json";
+  	$.ajax({
+		url: redditURL,
+		data: {
+			format: 'json'
+		},
+		success: function(response) {
+			console.log(response);
+		}  	
+	});
+
+})();
