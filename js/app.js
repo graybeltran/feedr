dfbd0d5955bf6f0ca5cdfded91804048ee5884b8
graybeltran <gray.beltran@gmail.com>
2015-12-20 13:55:12 -0500

    better handlebars

--- js/app.js
+++ js/app.js
@@ -44,40 +44,58 @@
 			format: 'json'
 		},
 		success: function(response) {
-			var source = $("#hello-world-template").html();
+			var source = $("#article-template").html();
 			var template = Handlebars.compile(source);
 
-			var helloStatement = {
-  				title: response.data.children[0].data.title, 
-  				thumbnail: response.data.children[0].data.thumbnail
-			};
-			var compiledTemplate = template(helloStatement);
-			$('.article').eq(0).find('h3').replaceWith(compiledTemplate);
-			$('.article').eq(0).find('.featuredImage img').replaceWith(compiledTemplate);
-			var helloStatementTwo = {
-  				title: response.data.children[1].data.title, 
-  				body: "This is my first post!"
-			};
-			var compiledTemplateTwo = template(helloStatementTwo);
-			$('.article').eq(1).find('h3').replaceWith(compiledTemplateTwo);
-			var helloStatementThree = {
-  				title: response.data.children[2].data.title, 
-  				body: "This is my first post!"
-			};
-			var compiledTemplateThree = template(helloStatementThree);
-			$('.article').eq(2).find('h3').replaceWith(compiledTemplateThree);
-			var helloStatementFour = {
-  				title: response.data.children[3].data.title, 
-  				body: "This is my first post!"
-			};
-			var compiledTemplateFour = template(helloStatementFour);
-			$('.article').eq(3).find('h3').replaceWith(compiledTemplateFour);
-			$('#popUp').addClass('hidden');
-			$('#popUp').addClass('hidden');
-			for (var i = 0; i < 25; i++) {
-				console.log(response.data.children[i].data);
-				$('body').append(response.data.children[i].data);
-			}
+			$(response.data.children).each(function() {
+				var articleData = {
+	  				title: this.data.title, 
+	  				thumbnail: this.data.thumbnail,
+	  				impressions: this.data.ups,
+	  				author: this.data.author,
+
+				};
+				var compiledTemplate = template(articleData);
+				$('#main').append(compiledTemplate);
+				$('#popUp').addClass('hidden');
+				console.log(response.data.children[0].data);
+			});
+
+
+			// var source = $("#hello-world-template").html();
+			// var template = Handlebars.compile(source);
+
+			// var helloStatement = {
+  	// 			title: response.data.children[0].data.title, 
+  	// 			thumbnail: response.data.children[0].data.thumbnail
+			// };
+			// var compiledTemplate = template(helloStatement);
+			// $('.article').eq(0).find('h3').replaceWith(compiledTemplate);
+			// $('.article').eq(0).find('.featuredImage img').replaceWith(compiledTemplate);
+			// var helloStatementTwo = {
+  	// 			title: response.data.children[1].data.title, 
+  	// 			body: "This is my first post!"
+			// };
+			// var compiledTemplateTwo = template(helloStatementTwo);
+			// $('.article').eq(1).find('h3').replaceWith(compiledTemplateTwo);
+			// var helloStatementThree = {
+  	// 			title: response.data.children[2].data.title, 
+  	// 			body: "This is my first post!"
+			// };
+			// var compiledTemplateThree = template(helloStatementThree);
+			// $('.article').eq(2).find('h3').replaceWith(compiledTemplateThree);
+			// var helloStatementFour = {
+  	// 			title: response.data.children[3].data.title, 
+  	// 			body: "This is my first post!"
+			// };
+			// var compiledTemplateFour = template(helloStatementFour);
+			// $('.article').eq(3).find('h3').replaceWith(compiledTemplateFour);
+			// $('#popUp').addClass('hidden');
+			// $('#popUp').addClass('hidden');
+			// for (var i = 0; i < 25; i++) {
+			// 	console.log(response.data.children[i].data);
+			// 	$('body').append(response.data.children[i].data);
+			// }
 		}  	
 	});
 })();
