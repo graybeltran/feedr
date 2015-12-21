53644cfee42ebe5e0959c22e8b386151ba991b52
graybeltran <gray.beltran@gmail.com>
2015-12-20 19:12:47 -0500

    final tweaks

--- js/app.js
+++ js/app.js
@@ -13,10 +13,13 @@
  		$('#popUp').removeClass('loader');
 		var title = $(this).find('h3').html();
 		var url = $(this).find('#url').html();
+		var domain = $(this).find('#domain').html();
 		console.log(title);
 		console.log(url);
 		$('#popUp .container h1').replaceWith('<h1>' + title + '</h1>');
 		$('#popUp .container .popUpAction').attr('href', url);
+		$('#popUp .container p').replaceWith('<p>' + domain + '</p>');
+
 	});
 
 	$('.closePopUp').on( "click", function() {
@@ -38,8 +41,8 @@
 	  				thumbnail: this.data.thumbnail,
 	  				impressions: this.data.ups,
 	  				author: this.data.author,
-	  				url: 'https://www.reddit.com' + this.data.permalink
-
+	  				url: 'https://www.reddit.com' + this.data.permalink,
+	  				domain: this.data.domain
 				};
 				var compiledTemplate = template(articleData);
 				$('#main').append(compiledTemplate);
@@ -47,39 +50,5 @@
 				console.log(response.data.children[0].data);
 			});
 		}	
-			// var source = $("#hello-world-template").html();
-			// var template = Handlebars.compile(source);
-
-			// var helloStatement = {
-  	// 			title: response.data.children[0].data.title, 
-  	// 			thumbnail: response.data.children[0].data.thumbnail
-			// };
-			// var compiledTemplate = template(helloStatement);
-			// $('.article').eq(0).find('h3').replaceWith(compiledTemplate);
-			// $('.article').eq(0).find('.featuredImage img').replaceWith(compiledTemplate);
-			// var helloStatementTwo = {
-  	// 			title: response.data.children[1].data.title, 
-  	// 			body: "This is my first post!"
-			// };
-			// var compiledTemplateTwo = template(helloStatementTwo);
-			// $('.article').eq(1).find('h3').replaceWith(compiledTemplateTwo);
-			// var helloStatementThree = {
-  	// 			title: response.data.children[2].data.title, 
-  	// 			body: "This is my first post!"
-			// };
-			// var compiledTemplateThree = template(helloStatementThree);
-			// $('.article').eq(2).find('h3').replaceWith(compiledTemplateThree);
-			// var helloStatementFour = {
-  	// 			title: response.data.children[3].data.title, 
-  	// 			body: "This is my first post!"
-			// };
-			// var compiledTemplateFour = template(helloStatementFour);
-			// $('.article').eq(3).find('h3').replaceWith(compiledTemplateFour);
-			// $('#popUp').addClass('hidden');
-			// $('#popUp').addClass('hidden');
-			// for (var i = 0; i < 25; i++) {
-			// 	console.log(response.data.children[i].data);
-			// 	$('body').append(response.data.children[i].data);
-			// }  	
 	});
 })();
